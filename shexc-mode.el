633d2bf31441d024b00567d9f9f831c1106cc6c1
ericprud <eric@w3.org>
2015-01-28 17:30:29 -0500

    ~ fixed comments

--- shexc-mode.el
+++ shexc-mode.el
@@ -43,22 +43,20 @@ For detail, see `comment-dwim'."
        (STRING_LITERAL2	(format "\"\\(?:[^\x22\ \x5C\ \xA\ \xD\ ]\\|%s\\|%s\\)*\"" ECHAR UCHAR)) ; #x22=" #x5C=\ #xA=new line #xD=carriage return
        (STRING_LITERAL_LONG1	(format "'''\\(?:\\(?:'\\|''\\)?\\([^'\\\\]\\|%s\\|%s\\)\\)*'''" ECHAR UCHAR))
        (STRING_LITERAL_LONG2	(format "\"\"\"\\(?:\\(?:\"\\|\"\"\\)?\\(?:[^\"\\\\]\\|%s\\|%s\\)\\)*\"\"\"" ECHAR UCHAR))
-
        (WS		"[\x20\ \x9\ \xD\ \xA\ ]") ; #x20=space #x9=character tabulation #xD=carriage return #xA=new line
        (ANON		(format "\\(?:\\[%s*\\]\\)" WS))
-       ; @@ disabled \x10000\ -\x2FFFF\ in PN_CHARS_BASE; slowed emacs too much.
-       ;(PN_CHARS_BASE	"\\(?:[A-Za-z\x00C0\ -\x00D6\ \x00D8\ -\x00F6\ \x00F8\ -\x02FF\ \x0370\ -\x037D\ \x037F\ -\x1FFF\ \x200C\ -\x200D\ \x2070\ -\x218F\ \x2C00\ -\x2FEF\ \x3001\ -\xD7FF\ \xF900\ -\xFDCF\ \xFDF0\ -\xFFFD\ \x10000\ -\xEFFFF\ ]\\)")
+       ;; @@ disabled \x10000\ -\xEFFFF\ in PN_CHARS_BASE; slowed emacs too much.
+       ;;(PN_CHARS_BASE	"\\(?:[A-Za-z\x00C0\ -\x00D6\ \x00D8\ -\x00F6\ \x00F8\ -\x02FF\ \x0370\ -\x037D\ \x037F\ -\x1FFF\ \x200C\ -\x200D\ \x2070\ -\x218F\ \x2C00\ -\x2FEF\ \x3001\ -\xD7FF\ \xF900\ -\xFDCF\ \xFDF0\ -\xFFFD\ \x10000\ -\xEFFFF\ ]\\)")
        (PN_CHARS_BASE	"\\(?:[A-Za-z\x00C0\ -\x00D6\ \x00D8\ -\x00F6\ \x00F8\ -\x02FF\ \x0370\ -\x037D\ \x037F\ -\x1FFF\ \x200C\ -\x200D\ \x2070\ -\x218F\ \x2C00\ -\x2FEF\ \x3001\ -\xD7FF\ \xF900\ -\xFDCF\ \xFDF0\ -\xFFFD\ ]\\)")
        (PN_CHARS_U	(format "\\(?:%s\\|_\\)" PN_CHARS_BASE))
        (PN_CHARS	(format "\\(?:%s\\|-\\|[0-9]\\|\x00B7\ \\|[\x0300\ -\x036F\ ]\\|[\x203F\ -\x2040\ ]\\)" PN_CHARS_U))
-       ;(BLANK_NODE_LABEL	(format "_:\(?:%s\\|[0-9]\\)\\(?:\\(?:%s\\|\\.\\)*%s\\)?" PN_CHARS_U PN_CHARS PN_CHARS))
        (BLANK_NODE_LABEL	(format "\\(?:_:\\(?:%s\\|[0-9]\\)\\(?:\\(?:%s\\|\\.\\)*%s\\)?\\)" PN_CHARS_U PN_CHARS PN_CHARS))
        (PN_PREFIX	(format "\\(?:%s\\(?:\\(?:%s\\|\\.\\)*%s\\)?\\)" PN_CHARS_BASE PN_CHARS PN_CHARS))
        (PNAME_NS	(format "\\(?:%s?:\\)" PN_PREFIX))
        (PN_LOCAL_ESC	"\\(?:\\\\[_~.-!$&'()*+,;=/?#@%]\\)")
        (PERCENT 	(format "\\(?:%%%s%s\\)" HEX HEX))
        (PLX		(format "\\(?:%s\\|%s\\)" PERCENT PN_LOCAL_ESC))
-       (PN_LOCAL	(format "\\(?:\\(?:%s\\|:\\|[0-9]\\|%s\\)\\(?:\\(?:%s\\|'.'\\|':'\\|%s\\)*\\(?:%s\\|':'\\|%s\\)\\)?\\)" PN_CHARS_U PLX PN_CHARS PLX PN_CHARS PLX))
+       (PN_LOCAL	(format "\\(?:\\(?:%s\\|:\\|[0-9]\\|%s\\)\\(?:\\(?:%s\\|\\.\\|:\\|%s\\)*\\(?:%s\\|:\\|%s\\)\\)?\\)" PN_CHARS_U PLX PN_CHARS PLX PN_CHARS PLX))
        (PNAME_LN	(format "%s%s" PNAME_NS PN_LOCAL))
        (PrefixedName	(format "\\(?:%s\\|%s\\)" PNAME_LN PNAME_NS))
        (BlankNode	(format "\\(?:%s\\|%s\\)" BLANK_NODE_LABEL ANON))
@@ -68,53 +66,51 @@ For detail, see `comment-dwim'."
        (SP		(format "\\(?:\\(?:[ \t\r\n]\\|%s\\)*\\)" comment)) ; "SP" scans better than "skip"
        )
   (setq shexc-highlights
-; overly-simlified building blocks, mostly from <http://www.w3.org/TR/turtle/#terminals>:
-;   PNAME_NS: \\(?:[a-zA-Z0-9_-]\\)*:
-;   PN_LOCAL: \\(?:[:a-zA-Z0-9_-]\\|%[0-9a-fA-F][0-9a-fA-F]\\|\\\\[_~.-!$&'()*+,;=/?#@%]\\)*
-;   semantic action label: %\\(?:[a-zA-Z0-9_-]*\\)
 	(list
-	 (list "\\(prefix\\|PREFIX\\|start\\|START\\)\\>" 1 font-lock-keyword-face t) ; how to do case insensitive?
+	 ;; keywords (how to do case insensitive?)
+	 (list "\\<\\(prefix\\|PREFIX\\|start\\|START\\|a\\)\\>" 1 font-lock-keyword-face t)
+	 ;; highlight some punctuation
 	 (list "\\([.?*+&]\\)" 1 font-lock-keyword-face t)
-	 (list "\\<\\(a\\)\\>" 1 font-lock-keyword-face t)
-         ;(list "\\(\\(?:[a-zA-Z0-9_]+\\):\\)" 1 font-lock-type-face t)
-	 ;(list ":\\(\\(?:[a-zA-Z0-9_]+\\)\\)[ ;.)?+*,#\n]" 1 font-lock-constant-face t)
 
-	 (list (format "\\(%s\\)%s" PNAME_NS PN_LOCAL) 1 font-lock-type-face t) ; capture (PNAME_NS)PN_LOCAL
-	 (list (format "%s\\(%s\\)" PNAME_NS PN_LOCAL) 1 font-lock-constant-face t) ; capture PNAME_NS(PN_LOCAL)
-
+	 ;; PREFIX (foo:) <bar>
+	 (list (format "\\(?:prefix\\|PREFIX\\)%s\\(%s\\)" SP PNAME_NS) 1 font-lock-type-face t)
+	 ;; (foo:)bar
+	 (list (format "\\(%s\\)%s" PNAME_NS PN_LOCAL) 1 font-lock-type-face t)
+	 ;; foo:(bar)
+	 (list (format "%s\\(%s\\)" PNAME_NS PN_LOCAL) 1 font-lock-constant-face t)
+	 ;; <foo>
 	 (list (format "\\(%s\\)" IRIREF) 1 font-lock-function-name-face t)
+	 ;; _:foo
 	 (list (format "\\(%s\\)" BlankNode) 1 font-lock-function-name-face t)
 
-         ; @SP*<Shape> | &SP*<Shape>
+	 ;; navigational labels are in variable-name-face.
+	 ;; @SP*<Shape> | &SP*<Shape>
 	 (list (format "\\([@&]%s%s\\)" SP IRIREF) 1 font-lock-variable-name-face t)
-	 ;(list (format "\\([@&]\\(?:[ \n\t]*\\|%s\\)*%s\\)" comment IRIREF) 1 font-lock-variable-name-face t)
-         ; @SP*my:Shape | &SP*my:Shape
+	 ;; @SP*my:Shape | &SP*my:Shape
 	 (list (format "\\([@&]%s%s\\)" SP PrefixedName) 1 font-lock-variable-name-face t)
-         ; <Shape>SP*{...}
+	 ;; <Shape>SP*{...}
 	 (list (format "\\(%s\\)%s{" IRIREF SP) 1 font-lock-variable-name-face t)
-         ; my:ShapeSP*{...}
+	 ;; my:ShapeSP*{...}
 	 (list (format "\\(%s\\)%s{" PrefixedName SP) 1 font-lock-variable-name-face t)
-         ; start=SP*<Shape>
+	 ;; start=SP*<Shape>
 	 (list (format "start%s=%s\\(%s\\)" SP SP IRIREF) 1 font-lock-variable-name-face t)
-         ; start=SP*my:Shape
+	 ;; start=SP*my:Shape
 	 (list (format "start%s=%s\\(%s\\)" SP SP PrefixedName) 1 font-lock-variable-name-face t)
+	 ;; "foo" 'foo' """foo\nb"a""r""" '''foo\nb'a''r'''
+	 (list (format "\\(%s\\)" String) 1 font-lock-string-face t)
 
-	 ; @@ doesn't work for long lines
-	 ;(list "\\(\\\".*?\\\"\\)" 1 font-lock-string-face t)
-	 (list (format "\\(%s\\)" String) 1 font-lock-string-face t) ; @@ There's some other string stuff going on somewhere
-         ; Bug: some trailing characters are highlighted; restricting comments regexp
-         ; ("\\(#.*\\)" 1 font-lock-comment-face t)
-	 ;(list "^\\s-*\\(#.*\\)" 1 font-lock-comment-face t)
+	 ;; Bug: some trailing characters are highlighted; restricting comments regexp so add '^'
+	 ;;(list (format "\\(%s\\)" comment) 1 font-lock-comment-face t)
 	 (list (format "^\\(%s\\)" comment) 1 font-lock-comment-face t)
 
-         ; semantic actions
+	 ;; semantic actions
 	 (list (format "\\(%s\\)" CODE) 1 font-lock-preprocessor-face t)
 	 )
 	)
   )
 
 ;;(define-generic-mode 'shexc-mode
-(define-derived-mode shexc-mode fundamental-mode
+(define-derived-mode shexc-mode prog-mode
   ;; setup tab key not working :/
   ;;(setq c-basic-offset 4)
 
@@ -128,6 +124,6 @@ For detail, see `comment-dwim'."
   (modify-syntax-entry ?\n "> b" shexc-mode-syntax-table)
 
   ;; description
-  "Mode for Notation 3 documents."
+  "Mode for ShExC documents."
 )
 
